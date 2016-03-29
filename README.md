005b30f17f6796fafcd8ffa4478c3abd27aa7512
Eric Freeman <clownhunter@gmail.com>
2016-03-28 19:48:27 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+shit
+==========
+
+Simple Http, I Think
+
+Spin up a server wherever just by typing `shit` into your console.
+
+Based on a [gist](https://gist.github.com/aksakalli/9191056) by [aksakalli](https://github.com/aksakalli).
---
+++ src/shit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace shit
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var location = Directory.GetCurrentDirectory();
+            using (var server = new Server(location))
+            {
+                Console.WriteLine("Server is running on this port: " + server.Port);
+                while (true)
+                {
+                    // keep server alive
+                }
+            }
+        }
+    }
+}
